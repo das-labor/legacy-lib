51de225b1d86537e2d04c3e3eec68b0a857d661f
hansi <hansi@3700fd43-cf5f-42b0-b99b-d023e050f9a6>
2013-04-07 00:51:57 +0000

    printing baud info shit!

--- lib-host/uart-host.c
+++ lib-host/uart-host.c
@@ -16,6 +16,46 @@
 
 int uart_fd;
 
+unsigned int baud_to_value(speed_t baud)
+{
+	 switch (baud) {
+		case B0:      return 0;
+		case B50:     return 50;
+		case B75: return 75;
+		case B110: return 110;
+		case B134: return 134;
+		case B150: return 150;
+		case B200: return 200;
+		case B300: return 300;
+		case B600: return 600;
+		case B1200: return 1200;
+		case B1800: return 1800;
+		case B2400: return 2400;
+		case B4800: return 4800;
+		case B9600: return 9600;
+		case B19200: return 19200;
+		case B38400: return 38400;
+		case B57600: return 57600;
+		case B115200: return 115200;
+		case B230400: return 230400;
+		case B460800: return 460800;
+		case B500000: return 500000;
+		case B576000: return 576000;
+		case B921600: return 921600;
+		case B1000000: return 1000000;
+		case B1152000: return 1152000;
+		case B1500000: return 1500000;
+		case B2000000: return 2000000;
+		case B2500000: return 2500000;
+		case B3000000: return 3000000;
+		case B3500000: return 3500000;
+		case B4000000: return 4000000;
+		default: break;
+     }
+     
+     return 0;
+}
+
 
 void uart_init(char *sport) {
 	int rc;
@@ -39,19 +79,10 @@ void uart_init(char *sport) {
 	// set serial options
 	tcgetattr(uart_fd, &options);
 
-//	#if defined(__WINDOWS__) | defined(__CYGWIN__)
-		/*
-		 * needed because cfsetspeed is not available on Windows.
-		 */
-		cfsetispeed(&options, UART_BAUD_RATE);
-		cfsetospeed(&options, UART_BAUD_RATE);
-//	#else
-		/*
-		* Replaced the above with the following in the hope that it fixes the init
-		* bug.
-		*/
-//		cfsetspeed(&options, UART_BAUD_RATE);
-//	#endif
+	//both needed because cfsetspeed is not available on Windows.
+	cfsetispeed(&options, UART_BAUD_RATE);
+	cfsetospeed(&options, UART_BAUD_RATE);
+
 
 	options.c_cflag &= ~(PARENB | CSTOPB | CSIZE);
 	options.c_cflag |= (CS8 | CLOCAL | CREAD);
@@ -71,7 +102,7 @@ void uart_init(char *sport) {
 		exit(EXIT_FAILURE);
 	}
 	else
-		debug(1, "Serial CAN communication established at xx Baud"); //XXX add code
+		debug(1, "Serial CAN communication established at %lu Baud", baud_to_value(cfgetospeed(&options)));
 }
 
 void uart_close() {
