22aabdd177ef72dd30ea8f06682df523714ac24c
pl4nkton <pl4nkton@3700fd43-cf5f-42b0-b99b-d023e050f9a6>
2014-12-07 14:52:19 +0000

    lib: can_message linux file ending

--- lib/can_message_converters.h
+++ lib/can_message_converters.h
@@ -1,3 +1,3 @@
 
-void can_message_from_can_message_raw(can_message *cmsg, can_message_raw *rmsg);
-void can_message_raw_from_can_message(can_message_raw *rmsg, can_message *cmsg);
+extern void can_message_from_can_message_raw(can_message *cmsg, can_message_raw *rmsg);
+extern void can_message_raw_from_can_message(can_message_raw *rmsg, can_message *cmsg);
